b0e4fbe25517368f673ec9dfe98f163745c4b40f
asciidisco <public@asciidisco.com>
2012-05-07 10:29:36 +0200

    Added the possibility to use a file as a destination target, added type checks for config attributes, fixed a mkdirp related variable typo

--- tasks/htmlmin.js
+++ tasks/htmlmin.js
@@ -13,29 +13,58 @@ module.exports = function(grunt) {
 
   // External libs.
   var minify = require('html-minifier').minify,
-      wrench = require('mkdirp');
+      mkdirp = require('mkdirp'),
+      path = require('path');
 
   // ==========================================================================
   // TASKS
   // ==========================================================================
 
   grunt.registerMultiTask('htmlmin', 'Minify html files with kangax html-minifier.', function() {
-    var files = grunt.file.expandFiles(this.file.src),
-        dest = this.file.dest,
+    var files = [],
+        dest = '',
         destMap = [],
-        destPath = '';
+        destPath = '',
+        destinationIsFile = false;
 
+    // check if config is valid
+    if (grunt.utils._.isString(this.file.dest) === false) {
+      grunt.fatal('Please specify the destination property (must be a string)');
+    }
+
+    if (grunt.utils._.isArray(this.file.src) === false && grunt.utils._.isString(this.file.src) === false) {
+      grunt.fatal('Please specify the source property (must be a string or an array)');
+    }
+
+    // assign config
+    dest = this.file.dest;
+    files = grunt.file.expandFiles(this.file.src)
+    
     // resolve the destination path (first try windows pathseparators, then *nix)
     destMap = dest.split('\\');
     if (destMap.length === 1) {
       destMap = destMap[0].split('/');
     }
 
+    // check if the destination property is a file
+    if (String(path.extname(path.basename(path.normalize(dest)))).trim() !== '') {
+      destinationIsFile = true;
+    }
+
+    // check if we have multiple files and a file destination
+    if (destinationIsFile === true && files.length > 1) {
+      grunt.fatal('Found multiple files to minfiy but only one file specified in the dest property. Please use a directory as a destination path, or specify one file to be minified.');
+    }
+
     // Write minified source files.
-    var minifiedFiles = grunt.helper('htmlmin', files, dest, grunt.config('htmlminifier'));
+    var minifiedFiles = grunt.helper('htmlmin', files, grunt.config('htmlminifier'));
     grunt.utils._.each(files, function (src, idx) {
-      // resolve the path (first try windows pathseparators, then *nix)
-      var path = [], destinationPath = [], file;
+      // resolve the path (first try windows path separators, then *nix)
+      var path = [], 
+          destinationPath = [], 
+          fileContents = grunt.file.read(files[idx]),
+          file;
+      
       path = files[idx].split('\\');
       if (path.length === 1) {
         path = path[0].split('/');
@@ -53,6 +82,11 @@ module.exports = function(grunt) {
       // extract the file name
       file = destinationPath.pop();
 
+      // extract the destination file name if destination property is a file
+      if (destinationIsFile === true) {
+        destinationPath.pop();
+      }
+
       // generate the destination path recursive
       destPath = destinationPath.join('/');
       mkdirp.sync(destPath, 0777);
@@ -60,7 +94,7 @@ module.exports = function(grunt) {
       // write out the minified file
       grunt.log.writeln('File "' + destPath + '/' + file + '" created.');
       grunt.file.write(destPath + '/' + file, minifiedFiles[idx]);
-      grunt.helper('min_max_info', minifiedFiles[idx], grunt.file.read(files[idx]));
+      grunt.helper('min_max_info', minifiedFiles[idx], fileContents);
     });
   });
 
@@ -68,7 +102,7 @@ module.exports = function(grunt) {
   // HELPERS
   // ==========================================================================
 
-  grunt.registerHelper('htmlmin', function(srcFiles, dest, options) {
+  grunt.registerHelper('htmlmin', function(srcFiles, options) {
     var files = String(srcFiles).split(','),
         minifiedFiles = [];
     if (!options) { options = {}; }
@@ -84,7 +118,7 @@ module.exports = function(grunt) {
     } catch(e) {
       // Something went wrong.
       grunt.warn(e.toString());
-	    grunt.warn('html-minifier found errors.', 10);
+      grunt.warn('html-minifier found errors.', 10);
     }
   });
 
