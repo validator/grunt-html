f91433d72bae208b9d15db89db0fce6c4dee6b13
will Farrell <will.farrell@gmail.com>
2013-09-23 12:53:41 -0600

    updated valid message to match jshint & csslint output style

--- tasks/html.js
+++ tasks/html.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
         return;
       }
       if (!result.length) {
-        grunt.log.writeln(files.length + ' file(s) valid...'+'OK'.green);
+        grunt.log.writeln('>> '.green + files.length + ' file' + (data.length === 1 ? '' : 's') + ' lint free.');
         done();
         return;
       } else {
