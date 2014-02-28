0c8028ff78f8178d2462f7c144be3dd3d3bd9eb2
Jörn Zaefferer <joern.zaefferer@gmail.com>
2014-02-28 13:55:43 +0100

    Build: Runs nodeunit as part of default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  grunt.registerTask('default', 'jshint');
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
 
 };
