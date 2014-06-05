validator/grunt-html

commit a004c5d639b802b1c73a09d2e07979dae3f843c5

Lib: Set default java stack size to 512k. Fixes #22.

lib/htmllint.js
@@ -11,7 +11,7 @@ module.exports = function(config, done) {
   var chunks = [];
 
   async.mapSeries(chunkify(config.files, maxChars), function(chunk, cb) {
-    exec('java -jar ' + jar + ' --format json ' + chunk, function(error, stdout, stderr) {
+    exec('java -Xss512k -jar ' + jar + ' --format json ' + chunk, function(error, stdout, stderr) {
        if (error) {
         cb(error);
         return;

commit c8b54d39e84045b936f2d4740181329c211d2955

Fixed the task in 64 bit systems Bug introduced by a004c5d639b802b1c73a09d2e07979dae3f843c5

lib/htmllint.js
@@ -1,6 +1,7 @@
 module.exports = function(config, done) {
   var path = require('path');
   var exec = require('child_process').exec;
+  var os = require('os');
   var chunkify = require('./chunkify');
   var async = require('async');
   var jar = __dirname + '/../vnu.jar';
@@ -16,7 +17,9 @@ module.exports = function(config, done) {
   var chunks = [];
 
   async.mapSeries(chunkify(config.files, maxChars), function(chunk, cb) {
-    exec('java -Xss512k -jar "' + jar + '" --format json ' + chunk, {
+
+    // call java, increasing the default stack size for ia32 systems and using the default setting for x64 systems
+    exec('java ' + (os.arch() === 'ia32' ? '-Xss512k ' : '') + '-jar "' + jar + '" --format json ' + chunk, {
       'maxBuffer' : maxBuffer
     }, function(error, stdout, stderr) {
        if (error) {