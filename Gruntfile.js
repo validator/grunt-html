51772d9bf49a55624f60349ba74da4cb1279c76b
XhmikosR <xhmikosr@gmail.com>
2015-03-03 19:31:57 +0200

    Actually remove the `htmllint` target.
    Tests are run via nodeunit so there's no need to run them twice.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,27 +4,6 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
-    htmllint: {
-      valid: 'test/valid.html',
-      force: {
-        options: {
-          force: true
-        },
-        src: 'test/*.html'
-      },
-      ignore: {
-        options: {
-          ignore: [
-            'An “img” element must have an “alt” attribute, except under certain conditions. For details, consult guidance on providing text alternatives for images.',
-            'Attribute “unknownattr” not allowed on element “img” at this point.',
-            'Start tag seen without seeing a doctype first. Expected “<!DOCTYPE html>”.',
-            'The “clear” attribute on the “br” element is obsolete. Use CSS instead.'
-          ]
-        },
-        src: 'test/*.html'
-      }
-    },
-
     nodeunit: {
       files: 'test/*.js'
     },
@@ -41,7 +20,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  grunt.registerTask('default', ['jshint', 'nodeunit', 'htmllint']);
+  grunt.registerTask('default', ['jshint', 'nodeunit']);
   grunt.registerTask('test', 'default');
 
 };
