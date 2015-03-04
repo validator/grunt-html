2b94293839ea747368abe175d71265bf802e66ee
Josh Hagins <hagins.josh@gmail.com>
2015-03-04 13:07:42 -0500

    Add test case for absoluteFilePathsForReporter

--- test/html_test.js
+++ test/html_test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var path = require('path');
+var testCase = require('nodeunit').testCase;
 var htmllint = require('../lib/htmllint');
 
 function run(test, config, expected, message) {
@@ -25,37 +26,70 @@ function run(test, config, expected, message) {
   });
 }
 
-exports.htmllint = {
-  'all': function(test) {
-    run(test, {
-      files: ['test/valid.html', 'test/invalid.html']
-    }, [
-      {
-        lastLine: 1,
-        lastColumn: 16,
-        message: 'Start tag seen without seeing a doctype first. Expected “<!DOCTYPE html>”.',
-        file: path.join('test', 'invalid.html')
-      },
-      {
-        lastLine: 9,
-        lastColumn: 96,
-        message: 'Attribute “unknownattr” not allowed on element “img” at this point.',
-        file: path.join('test', 'invalid.html')
-      },
-      {
-        lastLine: 9,
-        lastColumn: 96,
-        message: 'An “img” element must have an “alt” attribute, except under certain conditions. For details, consult guidance on providing text alternatives for images.',
-        file: path.join('test', 'invalid.html')
-      },
-      {
-        lastLine: 11,
-        lastColumn: 19,
-        message: 'The “clear” attribute on the “br” element is obsolete. Use CSS instead.',
-        file: path.join('test', 'invalid.html')
-      }
-    ], 'four errors from test/invalid.html');
-  },
+exports.htmllint = testCase({
+  'all': testCase({
+    'with relative paths': function(test) {
+      run(test, {
+        files: ['test/valid.html', 'test/invalid.html']
+      }, [
+        {
+          lastLine: 1,
+          lastColumn: 16,
+          message: 'Start tag seen without seeing a doctype first. Expected “<!DOCTYPE html>”.',
+          file: path.join('test', 'invalid.html')
+        },
+        {
+          lastLine: 9,
+          lastColumn: 96,
+          message: 'Attribute “unknownattr” not allowed on element “img” at this point.',
+          file: path.join('test', 'invalid.html')
+        },
+        {
+          lastLine: 9,
+          lastColumn: 96,
+          message: 'An “img” element must have an “alt” attribute, except under certain conditions. For details, consult guidance on providing text alternatives for images.',
+          file: path.join('test', 'invalid.html')
+        },
+        {
+          lastLine: 11,
+          lastColumn: 19,
+          message: 'The “clear” attribute on the “br” element is obsolete. Use CSS instead.',
+          file: path.join('test', 'invalid.html')
+        }
+      ], 'four errors from test/invalid.html');
+    },
+    'with absolute paths': function(test) {
+      run(test, {
+        files: ['test/valid.html', 'test/invalid.html'],
+        absoluteFilePathsForReporter: true
+      }, [
+        {
+          lastLine: 1,
+          lastColumn: 16,
+          message: 'Start tag seen without seeing a doctype first. Expected “<!DOCTYPE html>”.',
+          file: path.resolve(path.join('test', 'invalid.html'))
+        },
+        {
+          lastLine: 9,
+          lastColumn: 96,
+          message: 'Attribute “unknownattr” not allowed on element “img” at this point.',
+          file: path.resolve(path.join('test', 'invalid.html'))
+        },
+        {
+          lastLine: 9,
+          lastColumn: 96,
+          message: 'An “img” element must have an “alt” attribute, except under certain conditions. For details, consult guidance on providing text alternatives for images.',
+          file: path.resolve(path.join('test', 'invalid.html'))
+        },
+        {
+          lastLine: 11,
+          lastColumn: 19,
+          message: 'The “clear” attribute on the “br” element is obsolete. Use CSS instead.',
+          file: path.resolve(path.join('test', 'invalid.html'))
+        }
+      ], 'four errors from test/invalid.html');
+    }
+  }),
   'ignore': function(test) {
     run(test, {
       ignore: [
@@ -73,4 +107,4 @@ exports.htmllint = {
       }
     ], 'one error from test/invalid.html, other three were ignored');
   }
-};
+});
