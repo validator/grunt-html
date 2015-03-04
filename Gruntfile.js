5cf74e08b67dfff831fe4d38f2829b675cf20b09
Jörn Zaefferer <joern.zaefferer@gmail.com>
2015-03-04 11:11:47 +0100

    Build: Cleanup Gruntfile
    Closes #48

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,31 @@
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
 
-  // Project configuration.
   grunt.initConfig({
     htmllint: {
-      valid: "test/valid.html",
-      invalid: "test/*.html",
+      valid: 'test/valid.html',
+      invalid: 'test/*.html',
       ignore: {
         options: {
           ignore: 'The “clear” attribute on the “br” element is obsolete. Use CSS instead.'
         },
-        src: "test/*.html"
+        src: 'test/*.html'
       },
       invalidPhp: {
         options: {
           ignore: /XML processing instructions/
         },
-        src: "test/*.php"
+        src: 'test/*.php'
       }
     },
     nodeunit: {
-      files: ['test/**/*.js']
+      files: 'test/**/*.js'
     },
     jshint: {
       files: ['Grunfile.js', 'tasks/**/*.js', 'test/**/*.js'],
       options: {
-        jshintrc: ".jshintrc"
+        jshintrc: '.jshintrc'
       }
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'default'
     }
   });
 
