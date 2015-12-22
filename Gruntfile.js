472ae45245d08496e8f90b70d45cb58e484b37da
Matt Groeninger <mgroeninger@gmail.com>
2015-12-21 19:35:18 -0700

    All: Added jscs and reworked style
    Did not update test directory to avoid breaking tests (grunt is set to ignore)
    jscs is set for:
       idiomatic style
       no maximum line length
       use ' (single quote) as valid quotation mark

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,60 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function( grunt ) {
 
-  grunt.initConfig({
-    htmllint: {
-      valid: 'test/valid.html',
-      invalid: 'test/*.html',
-      ignore: {
-        options: {
-          ignore: 'The “clear” attribute on the “br” element is obsolete. Use CSS instead.'
+    grunt.initConfig({
+        htmllint: {
+            valid: 'test/valid.html',
+            invalid: 'test/*.html',
+            ignore: {
+                options: {
+                    ignore: 'The “clear” attribute on the “br” element is obsolete. Use CSS instead.'
+                },
+                src: 'test/*.html'
+            },
+            invalidPhp: {
+                options: {
+                    ignore: /XML processing instructions/
+                },
+                src: 'test/*.php'
+            },
+            checkstyle: {
+                options: {
+                    reporter: 'checkstyle'
+                },
+                src: 'test/*.html'
+            },
+            json: {
+                options: {
+                    reporter: 'json'
+                },
+                src: 'test/*.html'
+            }
         },
-        src: 'test/*.html'
-      },
-      invalidPhp: {
-        options: {
-          ignore: /XML processing instructions/
+        nodeunit: {
+            files: [ 'test/**/*.js', '!test/support/**/*.js' ]
         },
-        src: 'test/*.php'
-      },
-      checkstyle: {
-        options: {
-          reporter: 'checkstyle'
+        jshint: {
+            files: [ 'Gruntfile.js', 'lib/**/*.js', 'tasks/**/*.js', 'test/**/*.js' ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
         },
-        src: 'test/*.html'
-      },
-      json: {
-        options: {
-          reporter: 'json'
-        },
-        src: 'test/*.html'
-      }
-    },
-    nodeunit: {
-      files: ['test/**/*.js', '!test/support/**/*.js']
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'lib/**/*.js', 'tasks/**/*.js', 'test/**/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    }
-  });
+        jscs: {
+            files: [ 'Gruntfile.js', 'lib/**/*.js', 'tasks/**/*.js', '!test/**/*.js' ],
+            options: {
+                config: '.jscsrc',
+                verbose: true
+            }
+        }
+    });
 
-  grunt.loadTasks('tasks');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadTasks( 'tasks' );
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
+    grunt.loadNpmTasks( 'grunt-jscs' );
 
-  grunt.registerTask('test', ['jshint', 'nodeunit']);
-  grunt.registerTask('default', 'test');
+    grunt.registerTask( 'test', [ 'jscs', 'jshint', 'nodeunit' ]);
+    grunt.registerTask( 'default', 'test' );
 
 };
