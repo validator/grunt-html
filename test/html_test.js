bf5d17cd7801ec881ff44c7267a45a8238d470ed
XhmikosR <xhmikosr@gmail.com>
2018-09-02 11:17:53 +0300

    ES6-ify code base and switch style.
    So many spaces with the previous style it became tedious.

--- test/html_test.js
+++ test/html_test.js
@@ -1,19 +1,17 @@
-'use strict';
+const path = require('path');
+const htmllint = require('../lib/htmllint');
+const expectedResults = require('./support/expected_results');
 
-var path = require( 'path' );
-var htmllint = require( '../lib/htmllint' );
-var expectedResults = require( './support/expected_results' );
-
-function run( test, config, expected, message ) {
-  test.expect( 1 );
+function run(test, config, expected, message) {
+  test.expect(1);
   // tests here
-  htmllint( config, function( error, result ) {
-    if ( error ) {
+  htmllint(config, (error, result) => {
+    if (error) {
       throw error;
     }
     // copy only the properties we want to test
     // url property is absolute, system-dependend path
-    result = result.map( function( msg ) {
+    result = result.map((msg) => {
       return {
         file: msg.file,
         type: msg.type,
@@ -21,96 +19,98 @@ function run( test, config, expected, message ) {
         lastLine: msg.lastLine,
         lastColumn: msg.lastColumn
       };
-    } );
-    test.deepEqual( result, expected, message );
+    });
+    test.deepEqual(result, expected, message);
     test.done();
-  } );
+  });
 }
 
 exports.htmllint = {
   'all': {
-    'with relative paths': function( test ) {
-      var expected = expectedResults.invalid;
-      run( test, {
-        files: [ 'test/valid.html', 'test/invalid.html' ],
-        errorlevels: [ 'info', 'warning', 'error' ]
-      }, expected, 'four errors from test/invalid.html' );
+    'with relative paths': (test) => {
+      const expected = expectedResults.invalid;
+
+      run(test, {
+        files: ['test/valid.html', 'test/invalid.html'],
+        errorlevels: ['info', 'warning', 'error']
+      }, expected, 'four errors from test/invalid.html');
     },
-    'with absolute paths': function( test ) {
-      var expected = expectedResults.invalid.map( function( result ) {
+    'with absolute paths': (test) => {
+      const expected = expectedResults.invalid.map((result) => {
         return {
-          file: path.resolve( result.file ),
+          file: path.resolve(result.file),
           type: result.type,
           message: result.message,
           lastLine: result.lastLine,
           lastColumn: result.lastColumn
         };
-      } );
-      run( test, {
-        files: [ 'test/valid.html', 'test/invalid.html' ],
+      });
+
+      run(test, {
+        files: ['test/valid.html', 'test/invalid.html'],
         absoluteFilePathsForReporter: true,
-        errorlevels: [ 'info', 'warning', 'error' ]
-      }, expected, 'four errors from test/invalid.html' );
+        errorlevels: ['info', 'warning', 'error']
+      }, expected, 'four errors from test/invalid.html');
     }
   },
-  'empty': function( test ) {
-    run( test, {
+  'empty': (test) => {
+    run(test, {
       files: [],
-      errorlevels: [ 'info', 'warning', 'error' ]
-    }, [], '0 errors from 0 files' );
+      errorlevels: ['info', 'warning', 'error']
+    }, [], '0 errors from 0 files');
   },
-  'bad-encoding': function( test ) {
-    run( test, {
-        files: [ path.join( 'test', 'invalid-encoding.html' ) ],
-        errorlevels: [ 'info', 'warning', 'error' ]
-      }, [
+  'bad-encoding': (test) => {
+    run(test, {
+      files: [path.join('test', 'invalid-encoding.html')],
+      errorlevels: ['info', 'warning', 'error']
+    }, [
       {
-        file: path.join( 'test', 'invalid-encoding.html' ),
+        file: path.join('test', 'invalid-encoding.html'),
         type: 'error',
         message: 'Malformed byte sequence: “e1”.',
         lastLine: 5,
         lastColumn: 9
       },
       {
-        file: path.join( 'test', 'invalid-encoding.html' ),
+        file: path.join('test', 'invalid-encoding.html'),
         type: 'error',
         message: 'Malformed byte sequence: “e1”.',
         lastLine: 9,
         lastColumn: 18
       },
       {
-        file: path.join( 'test', 'invalid-encoding.html' ),
+        file: path.join('test', 'invalid-encoding.html'),
         type: 'error',
         message: 'Internal encoding declaration “iso-8859-1” disagrees with the actual encoding of the document (“utf-8”).',
         lastLine: 4,
         lastColumn: 74
       },
       {
-        file: path.join( 'test', 'invalid-encoding.html' ),
+        file: path.join('test', 'invalid-encoding.html'),
         type: 'error',
         message: 'Bad value “text/html; charset=iso-8859-1” for attribute “content” on element “meta”: “charset=” must be followed by “utf-8”.',
         lastLine: 4,
         lastColumn: 74
       }
-    ] );
+    ]);
   },
-  'ignore': function( test ) {
-    run( test, {
+  'ignore': (test) => {
+    run(test, {
       ignore: [
         'The "clear" attribute on the "br" element is obsolete. Use CSS instead.',
         'Start tag seen without seeing a doctype first. Expected “<!DOCTYPE html>”.',
         /attribute “[a-z]+” not allowed/i
       ],
-      files: [ 'test/valid.html', 'test/invalid.html' ],
-      errorlevels: [ 'info', 'warning', 'error' ]
+      files: ['test/valid.html', 'test/invalid.html'],
+      errorlevels: ['info', 'warning', 'error']
     }, [
       {
         lastLine: 9,
         lastColumn: 96,
         type: 'error',
         message: 'An “img” element must have an “alt” attribute, except under certain conditions. For details, consult guidance on providing text alternatives for images.',
-        file: path.join( 'test', 'invalid.html' )
+        file: path.join('test', 'invalid.html')
       }
-    ], 'one error from test/invalid.html, other three were ignored' );
+    ], 'one error from test/invalid.html, other three were ignored');
   }
 };
--- test/json_test.js
+++ test/json_test.js
@@ -1,22 +1,22 @@
-'use strict';
-
-var reporter = require( '../lib/reporters/json' );
-var expectedResults = require( './support/expected_results' );
+const reporter = require('../lib/reporters/json');
+const expectedResults = require('./support/expected_results');
 
 exports.json = {
   'json reporter': {
-    'when given empty result': function( test ) {
-      var result = [],
-        expected = '[]',
-        actual = reporter( result );
-      test.equal( actual, expected, 'Should return empty json array for empty result' );
+    'when given empty result': (test) => {
+      const result = [];
+      const expected = '[]';
+      const actual = reporter(result);
+
+      test.equal(actual, expected, 'Should return empty json array for empty result');
       test.done();
     },
-    'when given non-empty result': function( test ) {
-      var result = expectedResults.invalid,
-        expected = JSON.stringify( result ),
-        actual = reporter( result );
-      test.equal( actual, expected, 'Should report errors as json array' );
+    'when given non-empty result': (test) => {
+      const result = expectedResults.invalid;
+      const expected = JSON.stringify(result);
+      const actual = reporter(result);
+
+      test.equal(actual, expected, 'Should report errors as json array');
       test.done();
     }
   }
