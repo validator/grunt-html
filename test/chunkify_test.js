5f42e94f30336c2c4cf15f09dd04e0cd022fdca1
XhmikosR <xhmikosr@gmail.com>
2018-09-08 13:21:23 +0300

    Remove spaces from chunkify().

--- test/chunkify_test.js
+++ test/chunkify_test.js
@@ -2,12 +2,12 @@ const chunkify = require('../lib/chunkify');
 
 exports.chunkify = {
   'default': (test) => {
-    const files = ['./some/long/path/to/file.html', './some/long/path/to/file.html', './some/long/path/to/file.html'];
-    const chunked = ['"./some/long/path/to/file.html" "./some/long/path/to/file.html" ', '"./some/long/path/to/file.html" '];
-    const all = ['"./some/long/path/to/file.html" "./some/long/path/to/file.html" "./some/long/path/to/file.html" '];
+    const files = ['./some/long/path/to/1/file.html', './some/long/path/to/2/file.html', './some/long/path/to/3/file.html'];
+    const chunked = ['"./some/long/path/to/1/file.html" "./some/long/path/to/2/file.html"', '"./some/long/path/to/3/file.html"'];
+    const all = ['"./some/long/path/to/1/file.html" "./some/long/path/to/2/file.html" "./some/long/path/to/3/file.html"'];
 
-    test.deepEqual(chunkify(files, 64), chunked, 'Should split the file list of file in 2 chunks');
-    test.deepEqual(chunkify(files, 128), all, 'Should do a single chunk');
+    test.deepEqual(chunkify(files, 70), chunked, 'Should split the file list of file in 2 chunks');
+    test.deepEqual(chunkify(files, 120), all, 'Should do a single chunk');
     test.done();
   }
 };
