c4a3493d7925ee9f6d584590c592676b71d2231e
XhmikosR <xhmikosr@gmail.com>
2018-10-06 10:08:39 +0300

    Change length checks to be against a value.

--- tasks/html.js
+++ tasks/html.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
       if (error) {
         passed = force;
         grunt.log.error(error);
-      } else if (!result.length) {
+      } else if (result.length === 0) {
         grunt.log.ok(`${files.length} ${grunt.util.pluralize(files.length, 'file/files')} lint free.`);
       } else {
         passed = force;
