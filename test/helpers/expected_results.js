d2e82ea28a9f1623f277c1827598312298c5b075
XhmikosR <xhmikosr@gmail.com>
2021-09-04 18:33:14 +0300

    Rename variables

--- test/helpers/expected_results.js
+++ test/helpers/expected_results.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 
-const invalidHtml = path.normalize('test/fixtures/invalid.html');
+const file = path.normalize('test/fixtures/invalid.html');
 
 module.exports = {
   invalid: [
@@ -11,28 +11,28 @@ module.exports = {
       lastColumn: 16,
       type: 'error',
       message: 'Start tag seen without seeing a doctype first. Expected “<!DOCTYPE html>”.',
-      file: invalidHtml
+      file
     },
     {
       lastLine: 9,
       lastColumn: 96,
       type: 'error',
       message: 'Attribute “unknownattr” not allowed on element “img” at this point.',
-      file: invalidHtml
+      file
     },
     {
       lastLine: 9,
       lastColumn: 96,
       type: 'error',
       message: 'An “img” element must have an “alt” attribute, except under certain conditions. For details, consult guidance on providing text alternatives for images.',
-      file: invalidHtml
+      file
     },
     {
       lastLine: 11,
       lastColumn: 19,
       type: 'error',
       message: 'The “clear” attribute on the “br” element is obsolete. Use CSS instead.',
-      file: invalidHtml
+      file
     }
   ]
 };
