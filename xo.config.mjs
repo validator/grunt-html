880e41eb736a041316ce26c4591f96300d1783ed
XhmikosR <xhmikosr@gmail.com>
2025-11-02 09:41:23 +0200

    Update major devDependencies

---
+++ xo.config.mjs
@@ -0,0 +1,32 @@
+import globals from 'globals';
+
+const xoConfig = [
+  {
+    space: true,
+    rules: {
+      '@stylistic/comma-dangle': ['error', 'never'],
+      '@stylistic/function-paren-newline': ['error', 'multiline-arguments'],
+      '@stylistic/max-len': 'off',
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/operator-linebreak': ['error', 'after'],
+      '@stylistic/space-before-function-paren': ['error', 'never'],
+      'arrow-body-style': 'off',
+      'capitalized-comments': 'off',
+      'require-unicode-regexp': 'off',
+      'unicorn/filename-case': 'off',
+      'unicorn/no-anonymous-default-export': 'off',
+      'unicorn/prefer-module': 'off',
+      'unicorn/prevent-abbreviations': 'off'
+    }
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.mocha
+      }
+    }
+  }
+];
+
+export default xoConfig;
