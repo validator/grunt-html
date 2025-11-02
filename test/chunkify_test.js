7b9795a5ec22c5ab355363e169c58deb98caba10
XhmikosR <xhmikosr@gmail.com>
2025-11-02 09:32:31 +0200

    Fix deprecation warning on Node.js 24
    Change chunkify to return arrays and remove `shell: true`` from execFile

--- test/chunkify_test.js
+++ test/chunkify_test.js
@@ -11,10 +11,14 @@ describe('chunkify', () => {
       './some/long/path/to/3/file.html'
     ];
     const chunked = [
-      '"./some/long/path/to/1/file.html" "./some/long/path/to/2/file.html"',
-      '"./some/long/path/to/3/file.html"'
+      ['./some/long/path/to/1/file.html', './some/long/path/to/2/file.html'],
+      ['./some/long/path/to/3/file.html']
     ];
-    const all = ['"./some/long/path/to/1/file.html" "./some/long/path/to/2/file.html" "./some/long/path/to/3/file.html"'];
+    const all = [[
+      './some/long/path/to/1/file.html',
+      './some/long/path/to/2/file.html',
+      './some/long/path/to/3/file.html'
+    ]];
 
     assert.deepEqual(chunkify(files, 70), chunked, 'Should split the file list of file in 2 chunks');
     assert.deepEqual(chunkify(files, 120), all, 'Should do a single chunk');
